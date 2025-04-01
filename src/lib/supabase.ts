67bf6ea7d370c558aef26f6f6599151860504965
mohab1707 <m.ababsa@esi-sba.dz>
2025-04-01 19:33:02 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Replace with your Supabase project URL and Anon Key from the Supabase Dashboard > Settings > API
-const supabaseUrl = 'https://eygezanxkjzkierkxubh.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5Z2V6YW54a2p6a2llcmt4dWJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1MjY2MjgsImV4cCI6MjA1OTEwMjYyOH0.mAlOH8qBTGfG0UVF8xZt_5T2XGOzP5VxDrtCf5z33cI';
+const supabaseUrl = 'https://your-project-id.supabase.co';
+const supabaseAnonKey = 'your-anon-key';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
