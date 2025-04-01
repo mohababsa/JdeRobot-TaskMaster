00e5a1847bf2142065689666fdd89a7f819cc60b
mohab1707 <m.ababsa@esi-sba.dz>
2025-04-01 19:31:00 +0100

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,15 +9,13 @@ import { getFirestore } from 'firebase/firestore'; // Add Firestore
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDqSa7Jgvbs9LmPFYFqcKh0kG-Cue0mNc4",
-  authDomain: "taskmasterapp-9c23f.firebaseapp.com",
-  projectId: "taskmasterapp-9c23f",
-  storageBucket: "taskmasterapp-9c23f.firebasestorage.app",
-  messagingSenderId: "991885258961",
-  appId: "1:991885258961:web:2d00d1746bd0d9057e15f0",
-  measurementId: "G-SKQ6JNSMME"
-};
-
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-sender-id",
+  appId: "your-app-id",
+  };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
